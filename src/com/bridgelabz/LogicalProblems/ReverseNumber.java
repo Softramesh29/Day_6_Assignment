1f124535561845fd38ee69062aa5ed63e3f8754a
softramesh29 <softramesh29@gmail.com>
2022-04-19 16:31:41 +0530

    Added : New lines are added in reverse a number file

--- src/com/bridgelabz/LogicalProblems/ReverseNumber.java
+++ src/com/bridgelabz/LogicalProblems/ReverseNumber.java
@@ -1,5 +1,18 @@
 package com.bridgelabz.LogicalProblems;
 
-public class ReverseNumber {
+import java.util.Scanner;
 
+public class ReverseNumber {
+	public static void main(String args[]){
+		int num,reversed_Num=0;
+		Scanner scan=new Scanner(System.in);
+			System.out.print("Enter the number for find reverse: ");
+		num=scan.nextInt();
+		for( ; num!=0;){
+		reversed_Num=reversed_Num*10;
+		reversed_Num=reversed_Num+num%10;
+		num=num/10;
+		}
+		System.out.print("Reversed number of given number is: "+reversed_Num);
+		}
 }
